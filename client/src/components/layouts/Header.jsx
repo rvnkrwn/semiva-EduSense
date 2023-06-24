596e216dfb8784c23faf2acd0144800681fae7cc
Revan Kurniawan <rvnkrwn@gmail.com>
2023-06-25 04:51:15 +0700

    add middleware and quiz generator by open ai

--- client/src/components/layouts/Header.jsx
+++ client/src/components/layouts/Header.jsx
@@ -1,85 +1,128 @@
 import ColorMode from "../colorMode";
-import {Link} from "react-router-dom";
-import {useSelector} from "react-redux";
-import {useEffect, useState} from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import fetchDataWithToken from "../../services/setAuthorization";
 import Loading from "../Loading";
+import Swal from "sweetalert2";
 
 export default function Header() {
     const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
 
-    const handleL
+    const handleLogout = () => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire("Logout!", "Your account has been logout.", "success");
+                localStorage.removeItem("token");
+                localStorage.removeItem("isLoggedIn");
+            }
+        });
+    };
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 // Memanggil fungsi fetchDataWithToken untuk mendapatkan data dengan token
-                const response = await fetchDataWithToken('http://localhost:3000/api/user/get-user');
+                const response = await fetchDataWithToken(
+                    "http://localhost:3000/api/user/get-user"
+                );
 
                 // Menyimpan data ke state komponen
                 setData(response);
-                if (response) {
-                    setIsLoading(false);
-                }
+                setIsLoading(false);
             } catch (error) {
-                console.error('Error fetching data:', error);
+                console.error("Error fetching data:", error);
+                setIsLoading(false);
             }
         };
 
-        fetchData();
-    }, []);
+        if (isLoggedIn) {
+            fetchData();
+        } else {
+            setIsLoading(false);
+        }
+    }, [isLoggedIn]);
 
     if (isLoading) {
-        return (
-            <>
-                <Loading/>
-            </>
-        )
+        return <Loading />;
     }
 
     const loggedInMenu = (
         <ul className="menu menu-horizontal">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/profile">{data.full_name}</Link></li>
-            <li><Link to="/register">Register</Link></li>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            {data && (
+                <li>
+                    <Link to="/profile">{data.full_name}</Link>
+                </li>
+            )}
+            <li>
+                <Link to="#" onClick={() => handleLogout()}>
+                    Logout
+                </Link>
+            </li>
         </ul>
     );
 
     const loggedOutMenu = (
         <ul className="menu menu-horizontal">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/login">Login</Link>
+            </li>
+            <li>
+                <Link to="/register">Register</Link>
+            </li>
         </ul>
     );
-
 
     return (
         <>
             <div className="drawer">
-                <input id="my-drawer-3" type="checkbox" className="drawer-toggle"/>
+                <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col">
                     {/* Navbar */}
                     <div className="w-full navbar bg-base-300">
                         <div className="flex-none lg:hidden">
                             <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                     className="inline-block w-6 h-6 stroke-current">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                          d="M4 6h16M4 12h16M4 18h16"></path>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    className="inline-block w-6 h-6 stroke-current"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    ></path>
                                 </svg>
                             </label>
                         </div>
-                        <div className="flex-1 px-2 mx-2 md:text-xl tracking-wider font-bold">EduSense</div>
+                        <div className="flex-1 px-2 mx-2 md:text-xl tracking-wider font-bold">
+                            EduSense
+                        </div>
                         <div className="flex-none hidden lg:block">
                             {isLoggedIn ? loggedInMenu : loggedOutMenu}
                         </div>
-                        <ColorMode/>
+                        <ColorMode />
                     </div>
                 </div>
-                <div className="drawer-side" style={{zIndex: 99999}}>
+                <div className="drawer-side" style={{ zIndex: 99999 }}>
                     <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
                     {isLoggedIn ? loggedInMenu : loggedOutMenu}
                 </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,3 @@
-import Card from "../components/Card";
 import {Link} from "react-router-dom";
 import {useSelector} from "react-redux";
 import {useEffect, useState} from "react";
@@ -9,22 +8,26 @@ export default function Home() {
     const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+
     if (!isLoggedIn) {
-        window.location.href = '/login'
+        window.location.href = "/login";
     }
+
     useEffect(() => {
         const fetchData = async () => {
             try {
-                // Memanggil fungsi fetchDataWithToken untuk mendapatkan data dengan token
-                const response = await fetchDataWithToken('http://localhost:3000/api/user/get-user');
+                // Call fetchDataWithToken function to get data with token
+                const response = await fetchDataWithToken(
+                    "http://localhost:3000/api/user/get-user"
+                );
 
-                // Menyimpan data ke state komponen
+                // Save data to component state
                 setData(response);
                 if (response) {
                     setIsLoading(false);
                 }
             } catch (error) {
-                console.error('Error fetching data:', error);
+                console.error("Error fetching data:", error);
             }
         };
 
@@ -36,33 +39,54 @@ export default function Home() {
             <>
                 <Loading/>
             </>
-        )
+        );
     }
+
+    // Get the first 3 quizzes from the data
+    const quizzes = data.quizResults.slice(0, 3);
+
     return (
         <>
             <div className="contain md:bg-base-200 p-4 md:rounded-2xl">
-                <div className="titile-contain flex justify-between p-4 mb-4">
-                    <h1 className='text-xl md:text-2xl'>Available classes</h1>
-                    <Link to={"/"} className="text-sm self-center">Lihat Semua</Link>
+                <div className="titile-contain flex justify-between p-4 mb-4 border-b">
+                    <h1 className="text-xl md:text-2xl">List Quizzes</h1>
+                    <Link to={"/"} className="text-sm self-center">
+                        Lihat Semua
+                    </Link>
                 </div>
                 <div className="cards p-2 flex flex-wrap justify-center md:justify-start">
-                    <Link to={'/home'}>
-                        <div className="card w-72 bg-base-100 shadow-xl m-2">
-                            <figure className='h-60'><img src={'https://images.unsplash.com/photo-1687591222784-41b886039512?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80'} alt="Shoes" /></figure>
-                            <div className="card-body">
-                                <h2 className="card-title">
-                                    XII MIPA 7
-                                    <div className="badge badge-secondary">class</div>
-                                </h2>
-                                <p>Matematika</p>
-                                <div className="card-actions justify-end">
-                                    <div className="badge badge-outline">Mapel Wajib</div>
+                    {quizzes.length > 0 ? (
+                        quizzes.map((quiz) => (
+                            <Link to={`/quiz/${quiz.id}`} key={quiz.id}>
+                                <div className="card w-72 bg-base-100 shadow-xl m-2">
+                                    <figure className="h-60">
+                                        <img
+                                            src={
+                                                "https://images.unsplash.com/photo-1687591222784-41b886039512?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80"
+                                            }
+                                            alt="Shoes"
+                                        />
+                                    </figure>
+                                    <div className="card-body">
+                                        <h2 className="card-title">
+                                            {quiz.name}
+                                            <div className="badge badge-secondary">class</div>
+                                        </h2>
+                                        <p>{quiz.description}</p>
+                                        <div className="card-actions justify-end">
+                                            <div className="badge badge-outline">Mapel Wajib</div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+                            </Link>
+                        ))
+                    ) : (
+                        <div className="w-full text-center p-4">
+                            <p>No quizzes available.</p>
                         </div>
-                    </Link>
+                    )}
                 </div>
             </div>
         </>
-    )
+    );
 }
