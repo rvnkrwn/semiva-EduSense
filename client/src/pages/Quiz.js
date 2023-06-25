e3cb1d80f7c500d49e6e85e76e10865b854b2159
Revan Kurniawan <rvnkrwn@gmail.com>
2023-06-25 07:07:52 +0700

    fixed fitur generator quiz

--- client/src/pages/Quiz.js
+++ client/src/pages/Quiz.js
@@ -1,91 +1,125 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 import {useSelector} from "react-redux";
-import {useEffect, useState} from "react";
-import fetchDataWithToken from "../services/setAuthorization";
-import Loading from "../components/Loading";
 
-export default function Quiz() {
+const QuizForm = () => {
     const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-    const [command, setCommand] = useState("");
-    const [data, setData] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
     if (!isLoggedIn) {
-        window.location.href = '/login'
+        window.location.href = "/login";
     }
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                // Memanggil fungsi fetchDataWithToken untuk mendapatkan data dengan token
-                const response = await fetchDataWithToken('http://localhost:3000/api/user/get-user');
-
-                // Menyimpan data ke state komponen
-                setData(response);
-                if (response) {
-                    setIsLoading(false);
-                }
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    if (isLoading) {
-        return (
-            <>
-                <Loading/>
-            </>
-        )
-    }
-    const payload = {
-        code: '',
-        name: '',
-        description: '',
-        teacher: data._id,
-        question: [],
-        students: []
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Lakukan sesuatu dengan nilai command
-        console.log("Command yang dimasukkan:", command);
-        // Reset nilai command setelah submit
-        setCommand("");
+    const initialValues = {
+        prompt: "",
+        code: "",
+        name: "",
+        description: "",
+        teacher: "",
+        students: "",
     };
 
-    const handleChange = (e) => {
-        setCommand(e.target.value);
+    const validationSchema = Yup.object().shape({
+        prompt: Yup.string().required("Prompt is required"),
+        code: Yup.string().required("Code is required"),
+        name: Yup.string().required("Name is required"),
+        description: Yup.string().required("Description is required"),
+        teacher: Yup.string().required("Teacher ID is required"),
+        students: Yup.string().required("Students IDs are required"),
+    });
+
+    const handleSubmit = async (values, { setSubmitting }) => {
+        try {
+            const response = await axios.post("http://localhost:3000/api/openai/ask", values);
+            console.log("Response:", response.data);
+        } catch (error) {
+            console.error("Error:", error);
+        } finally {
+            setSubmitting(false);
+        }
     };
 
     return (
-        <div>
-            <h2 className="text-xl font-bold">Masukkan Command</h2>
-            <form onSubmit={handleSubmit}>
-            <textarea
-                className="border border-primary bg-base-200 rounded p-2 mt-2 w-full"
-                value={command}
-                onChange={handleChange}
-                style={{resize: 'none'}}
-                placeholder="Contoh: 5 soal matematika dan 7 soal ipa"
-                rows={4}
-            />
-                <div className='flex flex-col-reverse'>
-                    <small className='font-bold'>
-                        *noted: Harap Masukan jumlah soal dan materi soal saja, akan automatic membuat dan memiliki
-                        jawaban
-                    </small>
-                    <small className='italic'>
-                        *noted: Please enter only the number of questions and the question material. The system
-                        will automatically generate and provide the answers.
-                    </small>
-                </div>
-                <button
-                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 mt-2 rounded"
-                    type="submit"
-                >
-                    Submit
-                </button>
-            </form>
+        <div className="container mx-auto bg-base-200 p-8 rounded" style={{maxWidth: "400px"}}>
+            <h2 className="text-xl font-bold">Form Quiz</h2>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmit}
+            >
+                {({ isSubmitting }) => (
+                    <Form>
+                        <div className="my-2">
+                            <label htmlFor="prompt">Prompt:</label>
+                            <Field
+                                type="text"
+                                id="prompt"
+                                name="prompt"
+                                className="w-full input input-bordered input-primary"
+                            />
+                            <ErrorMessage name="prompt" component="div" className="text-red-500" />
+                        </div>
+                        <div className="my-2">
+                            <label htmlFor="code">Code:</label>
+                            <Field
+                                type="text"
+                                id="code"
+                                name="code"
+                                className="w-full input input-bordered input-primary"
+                            />
+                            <ErrorMessage name="code" component="div" className="text-red-500" />
+                        </div>
+                        <div className="my-2">
+                            <label htmlFor="name">Name:</label>
+                            <Field
+                                type="text"
+                                id="name"
+                                name="name"
+                                className="w-full input input-bordered input-primary"
+                            />
+                            <ErrorMessage name="name" component="div" className="text-red-500" />
+                        </div>
+                        <div className="my-2">
+                            <label htmlFor="description">Description:</label>
+                            <Field
+                                as="textarea"
+                                id="description"
+                                name="description"
+                                className="w-full input input-bordered input-primary"
+                            />
+                            <ErrorMessage name="description" component="div" className="text-red-500" />
+                        </div>
+                        <div className="my-2">
+                            <label htmlFor="teacher">Teacher:</label>
+                            <Field
+                                type="text"
+                                id="teacher"
+                                name="teacher"
+                                className="w-full input input-bordered input-primary"
+                            />
+                            <ErrorMessage name="teacher" component="div" className="text-red-500" />
+                        </div>
+                        <div className="my-2">
+                            <label htmlFor="students">Students:</label>
+                            <Field
+                                type="text"
+                                id="students"
+                                name="students"
+                                className="w-full input input-bordered input-primary"
+                            />
+                            <ErrorMessage name="students" component="div" className="text-red-500" />
+                        </div>
+                        <button
+                            className="bg-primary/80 hover:bg-primary text-white font-bold py-2 px-4 mt-2 rounded"
+                            type="submit"
+                            disabled={isSubmitting}
+                        >
+                            {isSubmitting ? "Submitting..." : "Submit"}
+                        </button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     );
-}
+};
+
+export default QuizForm;
